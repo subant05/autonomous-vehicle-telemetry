d4058780c17fe79c706a797cb103cf39c8ea3cea
Anthony Crawford <anthony.crawford@bluerivertech.com>
2021-05-18 19:38:17 -0500

    Added subscription the Devices page

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutes } from './routes/app-routes.module'
 import { ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
+import {APOLLO_OPTIONS} from 'apollo-angular';
+import {HttpLink} from 'apollo-angular/http';
+import {split, ApolloClientOptions} from '@apollo/client/core';
+import {WebSocketLink} from '@apollo/client/link/ws';
+import {getMainDefinition} from '@apollo/client/utilities';
+import {InMemoryCache} from '@apollo/client/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
@@ -11,41 +17,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Material
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
-import {OverlayModule} from '@angular/cdk/overlay';
 // 
 
 import { ProfileComponent } from './pages/profile/profile.component';
@@ -70,45 +42,54 @@ import { GraphQLModule } from './graphql.module';
     HttpClientModule,
     BrowserAnimationsModule,
     // Material
-    MatAutocompleteModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
     MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatTreeModule,
+
     GraphQLModule,
     // 
   ],
-  providers: [],
+  providers: [{
+    provide: APOLLO_OPTIONS,
+    // @ts-ignore
+    useFactory(httpLink: HttpLink): ApolloClientOptions {
+      // Create an http link:
+      const http = httpLink.create({
+        uri: 'http://localhost:4000/graphql',
+      });
+
+      // Create a WebSocket link:
+      const ws = new WebSocketLink({
+        uri: `ws://localhost:4000/graphql`,
+        options: {
+          reconnect: true,
+        },
+      });
+
+      // using the ability to split links, you can send data to each link
+      // depending on what kind of operation is being sent
+      const link = split(
+        // split based on operation type
+        // ({query}) => {
+        //   const {kind, operation} = getMainDefinition(query);
+        //   return (
+        //     kind === 'OperationDefinition' && operation === 'subscription'
+        //   );
+        // },
+        ({ query }) => {
+          let definition = getMainDefinition(query);
+          return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+        },
+        ws,
+        http,
+      );
+
+      return {
+        link,
+        cache: new InMemoryCache({}),
+        // ... options
+      };
+    },
+    deps: [HttpLink],
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/src/app/pages/devices/devices.component.ts
+++ app/src/app/pages/devices/devices.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
 
+const COMMENTS_SUBSCRIPTION = gql`
+subscription deviceMessage {
+  deviceMessage{
+    timestamp
+    topic
+    type
+    msg
+    
+  }
+}`;
 @Component({
   selector: 'app-devices',
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss']
 })
 export class DevicesComponent implements OnInit {
+  
+  constructor(apollo: Apollo) {
+    apollo.subscribe({
+      query: COMMENTS_SUBSCRIPTION // Subscription gql
+    }).subscribe(
+      (response:any) => {
+        console.log(response)
+      },
+      error => {
+        console.log(error);
+      }
+    );
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
