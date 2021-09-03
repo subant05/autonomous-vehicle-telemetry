1952fc81930f9fa55a2b5d956c0832c8c5b1182f
Anthony Crawford <anthony.crawford@bluerivertech.com>
2021-09-03 13:00:05 -0500

    feature/ROBOT-784: adjusted logging subscription

--- server/migrations/sqls/20210617163343-create-table-images-down.sql
+++ server/migrations/sqls/20210617163343-create-table-images-down.sql
@@ -33,3 +33,4 @@ DROP TABLE images.camera_message_header;
 DROP TABLE images.camera_message;
 DROP TABLE images.camera;
 DROP TABLE images.segmentation_map;
+DROP FUNCTION images.insert_camera_image;
