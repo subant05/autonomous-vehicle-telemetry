2b8a43caeef6d66b98a9a772cb4bbf8d7f47f4ca
Anthony Crawford <anthony.crawford@bluerivertech.com>
2021-09-16 12:58:12 -0500

    feature/ROBOT-784: added pagination to the mission stats

--- server/dist/main.js
+++ server/dist/main.js
@@ -4250,7 +4250,7 @@ class ImageComponent {
         this.imageLoaded = false;
         const imgSrc = event.path[0].src;
         event.path[0].src = "";
-        setTimeout(() => event.path[0].src = imgSrc, 1000);
+        setTimeout(() => event.path[0].src = imgSrc, 4000);
     }
     ngOnDestroy() {
         var _a;
