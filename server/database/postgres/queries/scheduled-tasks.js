f8edb743e518c3183458a1121491485dd5b98777
Anthony Crawford <anthony.crawford@bluerivertech.com>
2021-09-23 15:26:28 -0500

    fixing-image-loading: fixing performance issue

--- server/database/postgres/queries/scheduled-tasks.js
+++ server/database/postgres/queries/scheduled-tasks.js
@@ -14,7 +14,7 @@ export const removeOfflineVehicles = async ()=>{
         ]
     )
 
-    setTimeout(()=>removeOfflineVehicles(), 1000)
+    setTimeout(()=>removeOfflineVehicles(), 3000)
 }
 
 export const publishOnlineVehicles = async ()=>{
@@ -29,6 +29,6 @@ export const publishOnlineVehicles = async ()=>{
       )
     `)
 
-    setTimeout(()=>publishOnlineVehicles(), 500)
+    setTimeout(()=>publishOnlineVehicles(), 1000)
 
 }
