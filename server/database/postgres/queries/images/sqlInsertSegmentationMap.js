6ef265776e46d63af46220cd57e7fb254d3a52ce
Anthony Crawford <anthony.crawford@bluerivertech.com>
2021-09-23 16:48:48 -0500

    fixing-image-loading: fixing performance issue

--- server/database/postgres/queries/images/sqlInsertSegmentationMap.js
+++ server/database/postgres/queries/images/sqlInsertSegmentationMap.js
@@ -1,40 +1,37 @@
 import { sqlInsertTopic, sqlInsertTopicSequence } from '../topics'
 import {sqlInsertVehicle, sqlInsertVehicleOnline, sqlInsertVehicleTopic} from '../vehicles'
+import {formatDateTime} from '../_utils'
 import moment from 'moment';
 import {sqlInsertCameraMessage} from './sqlInsertCameraMessage'
 const { client, pool } = require("../../connection.js")
 
 export const sqlInsertSegmentationMap = async (argTopic, data, cb=a=>a) =>{
+    cb(null, JSON.stringify("Data Sent") )
+
     if(!argTopic.includes('/segmentation') || !data){
-        cb(null, "ignored")
+        // cb(null, "ignored")
         return;
     }
 
     try {
-        cb(null, "Data Recieved" )
 
         const topic = await sqlInsertTopic(argTopic, {category:"images", ...data})
         const vehicle = await sqlInsertVehicle(data.vehicle)
         const vehicleTopic = await sqlInsertVehicleTopic(vehicle.rows[0].id, topic.rows[0].id)
         const vehicleSequence = await sqlInsertTopicSequence(vehicle.rows[0].id, topic.rows[0].id, data)
         const vehicleOnline = await sqlInsertVehicleOnline(vehicle.rows[0].id)
-        const cameraMessage = await sqlInsertCameraMessage(data.msg)
-        const queryResult = await client.query(`
-            INSERT INTO images.segmentation_map 
-            (readingAt, topic_id, msg_id, vehicle_id)
-            VALUES(
-                $1
-                , $2
-                , $3 
-                , $4
-            )
+        const imageData = data.msg.image.data
+        data.msg.image.data = null
 
-            RETURNING id
-        `, [
-            moment(data.timestamp/1000000).utc().format('YYYY-MM-DD HH:mm:ssZZ')
+        const queryResult =  await client.query(`
+            select images.insert_segmentation_map($1, $2, $3, $4, $5, $6);
+        `,[
+            JSON.stringify(data)
+            , formatDateTime(data.msg.descriptor.timestamp)
+            , formatDateTime(data.timestamp)
             , topic.rows[0].id
-            , cameraMessage.rows[0].id
             , vehicle.rows[0].id
+            , JSON.stringify(imageData)
         ])
 
         // cb(null, JSON.stringify(queryResult) )
